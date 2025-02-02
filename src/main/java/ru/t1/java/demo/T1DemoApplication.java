590257da6dc292844826f7cb6cd69c65b8b05d3c
Dmitry Shakhaev <dmtry.shahaev@gmail.com>
2025-02-02 19:37:58 +0300

    Initial commit

---
+++ src/main/java/ru/t1/java/demo/T1DemoApplication.java
@@ -0,0 +1,13 @@
+package ru.t1.java.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T1DemoApplication.class, args);
+	}
+
+}
